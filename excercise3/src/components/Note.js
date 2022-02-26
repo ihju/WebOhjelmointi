ab83ec2cd050b150fdfb2524ff375b59312fd904
ihju <t0ihju00@students.oamk.fi>
2022-02-26 20:28:53 +0200

    Nyt näkyy ne kuvatkin 5/5

--- excercise3/src/components/Note.js
+++ excercise3/src/components/Note.js
@@ -3,8 +3,7 @@ import React from 'react';
 const Note = ({note}) => {
     return (
         <div>
-            <img  style={{ margin: "20px 0" }}
-        width="100px" src = {note.kuva} alt = {note.nimi}/>
+    <img  src = {`/kuvat/${note.kuva}`} />
         <li>Tuotteen nimi: {note.nimi}</li>
         <li>Hinta:  {note.hinta}</li>
         <li> Saatavuus: {note.varastossa}</li>
